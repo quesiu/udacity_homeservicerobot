ef5dbabd2e226a17f532efbb740ab5415871f92e
quesiu <quentin.siutkowski@gmail.com>
2021-03-20 04:56:05 +0000

    feat: Add SLAM testing script

--- Project_5/src/scripts/test_slam.sh
+++ Project_5/src/scripts/test_slam.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-xterm  -e  " source /opt/ros/kinetic/setup.bash; roslaunch  turtlebot_gazebo turtlebot_world.launch" &
+xterm  -e  " source /home/workspace/udacity_homeservicerobot/catkin_ws/devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-xterm  -e  " source /opt/ros/kinetic/setup.bash; roscore" & 
+xterm  -e  " source /home/workspace/udacity_homeservicerobot/catkin_ws/devel/setup.bash; rosrun gmapping slam_gmapping" & 
 sleep 5
-xterm  -e  " rosrun rviz rviz" 
+xterm  -e  " source /home/workspace/udacity_homeservicerobot/catkin_ws/devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm  -e  " source /home/workspace/udacity_homeservicerobot/catkin_ws/devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch"
+sleep 5
